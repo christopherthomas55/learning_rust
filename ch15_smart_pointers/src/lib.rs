b361db7b77fd66f59b5bbf8316d479e634afe3f9
Chris <christopherthomas55@gmail.com>
2025-04-14 12:01:03 -0500

    Finish ch15

--- ch15_smart_pointers/src/lib.rs
+++ ch15_smart_pointers/src/lib.rs
@@ -160,5 +160,29 @@ fn mem_leak_example() {
     // println!("a next item = {:?}", a.tail());
 }
 
+use std::rc::Weak;
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
 fn weak_ref_example() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    let branch = Rc::new(Node {
+        value: 5,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    
+    // Adding a weak reference with Rc:downgrade above prevents an infinite reference here
+    // Weak references don't prevent Rc pointers from dropping either
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
