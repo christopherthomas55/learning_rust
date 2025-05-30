fed1cc053e442d80e1ae40ae1312faed6fbb09b0
Chris <christopherthomas55@gmail.com>
2025-05-29 21:57:19 -0500

    More ch 21 work

--- ch21_webserver/src/main.rs
+++ ch21_webserver/src/main.rs
@@ -1,36 +1,43 @@
 use std::{
     fs,
     io::{BufReader, prelude::*},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration
 };
+use trpl;
 
-fn main() {
+fn main_async() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established!");
-        handle_connection(stream);
+        trpl::spawn_task(async move {handle_connection(stream).await});
     }
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-
+async fn handle_connection(mut stream: TcpStream) {
     let delimiter = "\r\n";
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("html_src/hello.html").unwrap();
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "html_src/hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            trpl::sleep(Duration::from_millis(5000)).await;
+            ("HTTP/1.1 200 OK", "html_src/hello.html")
+        },
+        _ => ("HTTP/1.1 404 NOT FOUND", "html_src/404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
     let response = 
-        format!("{status_line}{delimiter}Content-Length: {length}{delimiter}{delimiter}{contents}");
-    println!("Request: {http_request:#?}");
+            format!("{status_line}{delimiter}Content-Length: {length}{delimiter}{delimiter}{contents}");
     stream.write_all(response.as_bytes()).unwrap();
 }
 
+fn main() {
+    main_async();
+}
